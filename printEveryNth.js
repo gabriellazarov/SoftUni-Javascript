31681512c45ff5531d99389cf045c6fb6a242e48
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-01-19 23:01:04 +0200

    Delete printEveryNth.js

--- printEveryNth.js
+++
@@ -1,9 +0,0 @@
-function solve(input, n) {
-    let answer = [input[0]];
-    for (let i = 1; i < input.length; i++) {
-        if (i % n == 0) {
-            answer.push(input[i]);
-        }
-    }
-    return answer;
-}
