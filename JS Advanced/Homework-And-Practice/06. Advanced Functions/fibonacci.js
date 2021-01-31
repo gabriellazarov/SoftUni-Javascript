4c13d33ad888330f07b9a714420930f989355d13
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-01-31 17:06:47 +0200

    Add files via upload

---
+++ JS Advanced/Homework-And-Practice/06. Advanced Functions/fibonacci.js
@@ -0,0 +1,11 @@
+function solve() {
+    let num1 = 0;
+    let num2 = 1;
+    function fib() {
+        let result = num1 + num2;
+        num1 = num2;
+        num2 = result;
+        return num1;
+    };
+    return fib;
+}
