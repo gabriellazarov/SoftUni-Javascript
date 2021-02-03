e34734ff32c0ac34de89a0bc5cde8be454000718
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-02-03 11:06:43 +0200

    Add files via upload

---
+++ JS Advanced/Homework-And-Practice/06. Advanced Functions/08. Next Article/script.js
@@ -0,0 +1,12 @@
+function getArticleGenerator(articles) {
+    let base = articles;
+    function submit() {
+        if (base[0] != undefined) {
+            let div = document.getElementById('content');
+            let el = document.createElement('article');
+            el.textContent = base.shift();
+            div.appendChild(el);
+        }
+    }
+    return submit;
+}
