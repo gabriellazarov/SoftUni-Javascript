91e068f427b334686d70bc3de7f7daa8ee61e6d7
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-02-15 19:59:22 +0200

    Add files via upload

---
+++ JS Advanced/Homework-And-Practice/09. Prototypes and Inheritance/01. Array Extension/arrayExtension.js
@@ -0,0 +1,42 @@
+(function solve() {
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+
+    Array.prototype.skip = function (n) {
+        let result = [];
+        for (let i = n; i < this.length; i++) {
+            result.push(this[i]);
+        }
+        return result;
+    };
+
+    Array.prototype.take = function (n) {
+        let result = [];
+        for (let i = 0; i < n; i++) {
+            result.push(this[i]);
+        }
+        return result;
+    };
+
+    Array.prototype.sum = function () {
+        let result = 0;
+        for (let el of this) {
+            result += el;
+        }
+        return result;
+    };
+
+    Array.prototype.average = function () {
+        return this.sum() / this.length;
+    };
+}());
+
+
+let testArray = [1, 2, 3];
+
+console.log(testArray.last());
+console.log(testArray.skip(1));
+console.log(testArray.take(2));
+console.log(testArray.sum());
+console.log(testArray.average());
