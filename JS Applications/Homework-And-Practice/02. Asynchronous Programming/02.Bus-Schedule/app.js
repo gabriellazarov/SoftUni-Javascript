961e7d3c57ff3fb88234a087b94b1d84895d75ae
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-03-02 17:04:03 +0200

    Update app.js

--- JS Applications/Homework-And-Practice/02. Asynchronous Programming/02.Bus-Schedule/app.js
+++ JS Applications/Homework-And-Practice/02. Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,32 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    let id;
+
+    async function depart() {
+        let currentStop = document.getElementById('info').children[0];
+
+        if (currentStop.textContent == 'Not Connected') id = 'depot';
+
+        let response = await fetch('http://localhost:3030/jsonstore/bus/schedule/' + id);
+        let data = await response.json();
+
+        currentStop.textContent = 'Next stop ' + data.name;
+
+        document.getElementById('depart').disabled = true;
+        document.getElementById('arrive').disabled = false;
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function arrive() {
+        let currentStop = document.getElementById('info').children[0];
+
+        let response = await fetch('http://localhost:3030/jsonstore/bus/schedule/' + id);
+        let data = await response.json();
+
+        currentStop.textContent = 'Arriving at ' + data.name;
+        id = data.next;
+
+        document.getElementById('depart').disabled = false;
+        document.getElementById('arrive').disabled = true;
     }
 
     return {
