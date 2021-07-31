b0145f56424613defdc46143057e1f138de50eb4
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-07-31 19:21:14 +0300

    Add files via upload

--- Programming-Basics-JS/Homework-And-Practice/1va Lekciq/expression.js
+++ Programming-Basics-JS/Homework-And-Practice/1va Lekciq/expression.js
@@ -1,5 +1,5 @@
 function expression(){
-    let result = (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3;
+    let result = (3522 + 5253) * 23 - (2336 * 501 + 23432 - 6743) * 3;
     console.log(result)
 }
 expression()
